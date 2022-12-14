3eac34a6a6174526e58f4ae9d75e9cf7895923a8
Kefa698 <kefaombongi07@gmail.com>
2022-12-14 14:36:16 +0300

    set up roles

---
+++ deploy/04-set-governorcontract.js
@@ -0,0 +1,40 @@
+const { network, ethers } = require("hardhat")
+const {
+    developmentChains,
+    VERIFICATION_BLOCK_CONFIRMATIONS,
+    ADRESSZERO,
+} = require("../helper-hardhat-config")
+const { verify } = require("../utils/verify")
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
+    const { deploy, log, get } = deployments
+    const { deployer } = await getNamedAccounts()
+    const governor = await ethers.getContract("GovernorContract", deployer)
+    const timelock = await ethers.getContract("TimeLock", deployer)
+    const waitBlockConfirmations = developmentChains.includes(network.name)
+        ? 1
+        : VERIFICATION_BLOCK_CONFIRMATIONS
+    log("-----------------------------------------------------")
+    log("Setting up contracts for roles...................")
+    //set roles
+    const proposerRole = await timelock.PROPOSER_ROLE()
+    const executorRole = await timelock.EXECUTOR_ROLE()
+    const adminRole = await timelock.TIMELOCK_ADMIN_ROLE()
+
+    const proposerTx = await timelock.grantRole(proposerRole, governor.address)
+    await proposerTx.waitBlockConfirmations
+    const executorTx = await timelock.grantRole(executorRole, ADRESSZERO)
+    await executorTx.waitBlockConfirmations
+    const revokeTx = await timelock.revokeRole(adminRole, deployer)
+    await revokeTx.waitBlockConfirmations
+    //  Now, anything the timelock wants to do has to go through the governance process!
+
+    // Verify the deployment
+    if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+        log("Verifying...")
+        await verify(GovernorContract.address, args)
+    }
+    log("roles set........................................")
+    log("----------------------------------------------------")
+}
+module.exports.tags = ["all", "setupContracts"]
