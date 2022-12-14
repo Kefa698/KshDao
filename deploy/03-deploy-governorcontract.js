d80ccc0f391caad4b769d5c4d2a0bd626ca898d0
Kefa698 <kefaombongi07@gmail.com>
2022-12-14 14:09:49 +0300

    deploy governorcontract

---
+++ deploy/03-deploy-governorcontract.js
@@ -0,0 +1,41 @@
+const { network, ethers } = require("hardhat")
+const {
+    developmentChains,
+    VERIFICATION_BLOCK_CONFIRMATIONS,
+    VOTING_DELAY,
+    VOTING_PERIOD,
+    QUARAM_PERCANTAGE,
+    ADRESSZERO,
+} = require("../helper-hardhat-config")
+const { verify } = require("../utils/verify")
+module.exports = async ({ getNamedAccounts, deployments }) => {
+    const { deploy, log, get } = deployments
+    const { deployer } = await getNamedAccounts()
+    const governanceToken = await get("GovernanceToken")
+    const timelock = await get("Timelock")
+
+    const waitBlockConfirmations = developmentChains.includes(network.name)
+        ? 1
+        : VERIFICATION_BLOCK_CONFIRMATIONS
+
+    log("----------------------------------------------------")
+    const args = [
+        governanceToken.address,
+        timelock.address,
+        VOTING_DELAY,
+        VOTING_PERIOD,
+        QUARAM_PERCANTAGE,
+    ]
+    const GovernorContract = await deploy("GovernorContract", {
+        from: deployer,
+        args: args,
+        log: true,
+        waitConfirmations: waitBlockConfirmations,
+    })
+    // Verify the deployment
+    if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+        log("Verifying...")
+        await verify(GovernorContract.address, arguments)
+    }
+    log("----------------------------------------------------")
+}
